e59eb110e712c4986d1aa3e65324512ab77924c0
djmisieq <zadroga.d@gmail.com>
2025-02-27 14:37:41 +0100

    Move Settings.js to archived folder

---
+++ src/archived/Settings.js
@@ -0,0 +1,13 @@
+// Ten plik został zastąpiony zintegrowanymi komponentami w App.js
+// Komponenty zostały podzielone na osobne widoki w głównym pliku aplikacji
+// Plik został zachowany dla referencji w folderze archived
+
+// Przeniesiono do archived: 27-02-2025
+
+import React from 'react';
+
+function Settings() {
+  return null;
+}
+
+export default Settings;
